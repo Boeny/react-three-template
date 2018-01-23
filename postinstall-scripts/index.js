3076f0bda8ca2701b399169dc81925556a0fd657
Alexander Sychev <alsy@dev11.office.sletat.int>
2018-01-23 09:31:43 +0300

    fixed finally!

--- postinstall-scripts/index.js
+++ postinstall-scripts/index.js
@@ -2,12 +2,13 @@ const fix = require('./content-fixer').default;
 
 fix(
   __dirname + '/../node_modules/react-three-renderer/lib/React3.js',
-  '(_react2.default.Component), _class.propTypes = {',
-  '(_react2.default.Component), _class.propTypes = {onKeyDown: _propTypes2.default.func,onKeyUp: _propTypes2.default.func,onMouseDown: _propTypes2.default.func,onMouseUp: _propTypes2.default.func,onClick: _propTypes2.default.func,'
+  "return _react2.default.createElement('canvas', {",
+  "return _react2.default.createElement('canvas', {onKeyDown: this.props.onKeyDown,onKeyUp: this.props.onKeyUp,onMouseDown: this.props.onMouseDown,onMouseUp: this.props.onMouseUp,onClick: this.props.onClick,"
 );
 
 fix(
   __dirname + '/../node_modules/react-three-renderer/lib/React3.js',
-  "return _react2.default.createElement('canvas', {",
-  "return _react2.default.createElement('canvas', {onKeyDown: this.props.onKeyDown,onKeyUp: this.props.onKeyUp,onMouseDown: this.props.onMouseDown,onMouseUp: this.props.onMouseUp,onClick: this.props.onClick,"
+  '_react2.default.Component), _class.propTypes = {',
+  '_react2.default.Component), _class.propTypes = {onKeyDown: _propTypes2.default.func,onKeyUp: _propTypes2.default.func,onMouseDown: _propTypes2.default.func,onMouseUp: _propTypes2.default.func,onClick: _propTypes2.default.func,'
 );
+
